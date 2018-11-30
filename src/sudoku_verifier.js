175c491fe878ed6f9c0cc29b193d794326160966
LindsayBrown81 <LindsayBrown81@users.noreply.github.com>
2018-11-30 16:26:24 -0500

    Story1: temporary edits to input of JSON to bypass webpack and view in local browser

--- src/sudoku_verifier.js
+++ src/sudoku_verifier.js
@@ -1,19 +1,199 @@
-export function sudokuVerifier({ problem, solution }) {
+// export function sudokuVerifier({ problem, solution }) {
 
-	// TODO implement this function
+(function wrap(){
+  //When called with the second sudoku problem and a valid solution it should have a valid status and an empty invalidIndexes array
+  function sudokuVerifier(json){
+  // TODO implement this function
+  var problem = "";
+  var solution = "";
+	var json={
+	"problem": [
+	1,
+	null,
+	null,
+	4,
+	8,
+	9,
+	null,
+	null,
+	6,
+	7,
+	3,
+	null,
+	null,
+	null,
+	null,
+	null,
+	4,
+	null,
+	null,
+	null,
+	null,
+	null,
+	null,
+	1,
+	2,
+	9,
+	5,
+	null,
+	null,
+	7,
+	1,
+	2,
+	null,
+	6,
+	null,
+	null,
+	5,
+	null,
+	null,
+	7,
+	null,
+	3,
+	null,
+	null,
+	8,
+	null,
+	null,
+	6,
+	null,
+	9,
+	5,
+	7,
+	null,
+	null,
+	9,
+	1,
+	4,
+	6,
+	null,
+	null,
+	null,
+	null,
+	null,
+	null,
+	2,
+	null,
+	null,
+	null,
+	null,
+	null,
+	3,
+	7,
+	8,
+	null,
+	null,
+	5,
+	1,
+	2,
+	null,
+	null,
+	4
+	],
+	"solution": [
+	1,
+	null,
+	null,
+	4,
+	8,
+	9,
+	3,
+	null,
+	6,
+	7,
+	3,
+	9,
+	null,
+	null,
+	6,
+	null,
+	4,
+	1,
+	4,
+	6,
+	8,
+	null,
+	null,
+	1,
+	2,
+	9,
+	5,
+	null,
+	null,
+	7,
+	1,
+	2,
+	null,
+	6,
+	null,
+	9,
+	5,
+	null,
+	1,
+	7,
+	null,
+	3,
+	null,
+	null,
+	8,
+	null,
+	null,
+	6,
+	null,
+	9,
+	5,
+	7,
+	null,
+	3,
+	9,
+	1,
+	4,
+	6,
+	null,
+	null,
+	null,
+	null,
+	null,
+	null,
+	2,
+	null,
+	null,
+	null,
+	8,
+	null,
+	3,
+	7,
+	8,
+	7,
+	null,
+	5,
+	1,
+	2,
+	null,
+	null,
+	4
+	]}
+	console.log("json ", json);
+	
+	problem = json.problem;
+	solution = json.solution;
+	// console.log("L json.problem: ", problem);
+	// console.log("L json.solution ", solution);
 
 	// store functions' output; i.e., game's status and index positions of invalid entries
-	var status = "status";
+	var status = '';
 	var invalidIndexes = [];
 
 	function rowChecker(problem, solution){
+		console.log("problem ", problem);
+		console.log("solution ", solution);
 		// for loop that checks for dups
-		for(var row = 0; row < 9; row ++){
+		for(var row = 0; row < 9; row++){
 			// scope of histories is per row
 			var indexHistory = [];
 			var valHistory = [];			
 			
-			for(var column = 0; column < 9; column ++){
+			for(var column = 0; column < 9; column++){
 				var index = column + (row * 9); 
 				indexHistory.push(index);				
 				
@@ -42,7 +222,7 @@ export function sudokuVerifier({ problem, solution }) {
 
 	function columnChecker(problem, solution){
 		// for loop that checks for dups
-		for(var column = 0; column < 9; column ++){
+		for(var column = 0; column < 9; column++){
 			var indexHistory = [];
 			var valHistory = [];
 			
@@ -73,13 +253,13 @@ export function sudokuVerifier({ problem, solution }) {
 
 	function squareChecker(solution) {
 		// for loop that checks for dups
-		for(var square = 0; square < 9; square ++){
+		for(var square = 0; square < 9; square++){
 			var indexHistory = [];
 			var valHistory = [];
 			
-			for(var row = 0; row < 3; row ++){
+			for(var row = 0; row < 3; row++){
 				
-				for(var column = 0; column < 3; column ++){
+				for(var column = 0; column < 3; column++){
 
 					var index =  column + (row * 9) + (square % 3);
 					indexHistory.push(index);				
@@ -91,9 +271,7 @@ export function sudokuVerifier({ problem, solution }) {
 							invalidIndexes.push(index);
 						}
 						
-						var origIndex = valHistory.indexOf(
-								solution[index]
-							);
+						var origIndex = valHistory.indexOf(solution[index]);
 						if(invalidIndexes.indexOf(indexHistory[origIndex]) === -1 && problem[indexHistory[origIndex]] == null){
 							invalidIndexes.push(indexHistory[origIndex]);
 						}
@@ -110,36 +288,43 @@ export function sudokuVerifier({ problem, solution }) {
 	rowChecker(problem, solution); //~ why are these args not colored? Do I need to pass in args?
 	columnChecker(problem, solution);
 	squareChecker(problem, solution);
+    
 	
 	// define game status
-	// if invalidIndexes is an array and is not an empty array
- 	if (Array.isArray(invalidIndexes) && invalidIndexes.length) {
+	// if invalidIndexes is an array and is not an empty array //removed Array.isArray(invalidIndexes) && 
+ 	if (Array.isArray(invalidIndexes) && invalidIndexes.length > 0) {
+ 		console.log("invalidIndexes.length", invalidIndexes.length);
 		status = "invalid";
+		console.log("invalidIndexes: ", invalidIndexes);
+		invalidIndexes.sort(); // ~ to match Jasmine tests
+		// return invalidIndexes;
 	}
 	
-	// if any of the functions above return a null value, then the status should be incomplete.
-	function hasEmptyValue(solution){ //pseudo
-		// forEach var value in solution
-		// 	if (value == null){
-		// 		return true;
-		// 	}
-		 for (const value of solution){
-		 	if (value == null){
-		 		return true;
-		 	}
-		 }
-	}
+	// // if any of the functions above return a null value, then the status should be incomplete.
+	// function hasEmptyValue(solution){ 
+	// 	for (const value of solution){
+	// 		if (value == null){
+	// 			return true;
+	// 	 	}
+	// 	}
+	// }
 
-	if (hasEmptyValue(solution) && status !== "invalid"){
-			status = "incomplete"
-		}
+	// if (hasEmptyValue(solution) && status !== "invalid"){
+	// 	status = "incomplete";
+	// 	invalidIndexes = 0;//or undefined;
+	// }
 
-	console.log("invalidIndexes: ", invalidIndexes);
-	invalidIndexes.sort(); // ~ what was I thinking here?
+	   	
+ //   	if ((!hasEmptyValue(solution)) && status !== "invalid"){
+	// 	status = "valid";
+	// 	invalidIndexes = 0;//or undefined;
+ //   	}                    
 
-  	return {
-  		status: status, //invalidIndexes, // valid, invalid, incomplete. invalid takes precedence over incomplete. so if its both, just say its invalid. 
-	    invalidIndexes: invalidIndexes, // which indexes are invalid ~ Delete comma? Proly not
-  	}
-}
+ //  	return {
+ //  		status: status, //invalidIndexes, // valid, invalid, incomplete. invalid takes precedence over incomplete. so if its both, just say its invalid. 
+	//     invalidIndexes: invalidIndexes, // which indexes are invalid ~ Delete comma? Proly not
+ //  	}
+  }
+  sudokuVerifier();
+})(); // closes function wrap  ~ semicolons?
 
